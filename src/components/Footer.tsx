e8b892fb08508ae7f31e5c6986ead3dbdf8d65c0
Steven Santana <steven.santana.s19@gmail.com>
2025-05-06 22:38:48 -0500

    commit about page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,15 @@ import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-300 text-center py-6 mt-12">
-        <h2 className="text-2xl font-semibold mb-2">Contáctame</h2>
-      <div className="flex flex-col items-center gap-2 mb-2 text-base">
-        <div className="flex gap-4" >
+    <footer className="bg-gray-900 text-gray-300 text-center py-6 mt-6 rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">Contáctame</h2>
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4 text-base mb-4">
         <a
           href="https://github.com/SantanaStev19"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 hover:text-white transition"
-        >  
+        >
           <FaGithub /> <span>SantanaStev19</span>
         </a>
         <a
@@ -28,13 +27,11 @@ export default function Footer() {
         >
           <FaEnvelope /> <span>steven.santana.s19@gmail.com</span>
         </a>
-        
-        </div>
-        
       </div>
       <p className="text-sm">
-        © {new Date().getFullYear()} [Tu Nombre]. Todos los derechos reservados.
+        © {new Date().getFullYear()} Jorge Steven Santana Salazar. Todos los derechos reservados.
       </p>
     </footer>
   )
 }
+
